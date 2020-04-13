2456189b0be4dcc438c76067d8832d0a1b8ab277
dukyeon ha <iribbonkr@gmail.com>
2020-04-13 21:50:07 +0900

    prop-types

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import Potato from './Potato';
+import PropTypes from "prop-types";
+
+function Food({fav,picture,rating}){
+  // console.log(props);
+  return(
+    <>
+    <h3>I love {fav}</h3>
+    <div>{rating} / 5</div>
+    <img src={picture} alt={fav}/>
+    </>
+  )
+}
+Food.propTypes = {
+  fav:PropTypes.string.isRequired,
+  picture:PropTypes.string.isRequired,
+  rating:PropTypes.number.isRequired
+}
+
+const foodLike = [
+  {
+    id:1,
+    fav:'kimchi',
+    picture:'https://images.unsplash.com/photo-1542296332-2e4473faf563?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    rating:5
+  },
+  {
+    id:2,
+    fav:'ramen',
+    picture:'https://images.unsplash.com/photo-1561131668-f63504fc549d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1296&q=60',
+    rating:3
+  },
+  {
+    id:3,
+    fav:'choco',
+    picture:'https://images.unsplash.com/photo-1530521954074-e64f6810b32d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60',
+    rating:2.5
+  },
+  {
+    id:4,
+    fav:'candy',
+    picture:'https://images.unsplash.com/photo-1561101904-da649fcbf03f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60',
+    rating:2
+  }
+]
+
+// function renderFood(dish){
+//   // console.log(dish)
+//   return <Food fav={dish.fav} picture={dish.picture} />
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* {console.log(foodLike.map(renderFood))} */}
+      {/* {foodLike.map(renderFood)} */}
+      {foodLike.map(dish => <Food key={dish.id} fav={dish.fav} picture={dish.picture} rating={dish.rating}/>)}
+      {/* <Food fav="kimchi" // something={true}
+      />
+      <Food fav="ramen"/>
+      <Food fav="choco"/>
+      <Food fav="candy"/> */}
     </div>
   );
 }
